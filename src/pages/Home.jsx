90d96644c02b3443d6a588a934ea6616af09989e
wlals262 <jgm262@naver.com>
2024-05-02 17:50:05 +0900

    feat: 메인페이지 상단탭 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+
+const menuIcon = require('../assets/icons/menu.png');
 
 const Home = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View style={{height: 300, backgroundColor: 'red'}}>
-          <Text>test</Text>
-        </View>
-        <View style={{height: 300, backgroundColor: 'blue'}}>
-          <Text>test</Text>
-        </View>
-        <View style={{height: 300, backgroundColor: 'green'}}>
-          <Text>test</Text>
-        </View>
-        <View style={{height: 300, backgroundColor: 'yellow'}}>
-          <Text>test</Text>
+        <View
+          style={{
+            height: 68,
+            backgroundColor: '#80D7CE',
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            paddingHorizontal: 24,
+          }}>
+          <View style={{flexDirection: 'row', columnGap: 8}}>
+            <Text>사진</Text>
+            <Text>admin 님</Text>
+          </View>
+          <TouchableOpacity>
+            <Image source={menuIcon} style={{width: 24, height: 24}} />
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
