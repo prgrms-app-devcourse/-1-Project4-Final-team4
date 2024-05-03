5c50b1fd26150192d850e0e7470bd0701afddc63
moko <ljun925@naver.com>
2024-05-03 19:17:53 +0900

    chore: 컨벤션에 맞게 수정

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/router.js';
+import 'react-native-devsettings';
 
 function App(): React.JSX.Element {
   return (
--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,4 +1,9 @@
-export const Colors = {
+const Colors = {
+  main: '#80D7CE',
+  background: '#FEFBF6',
+  black: '#313131',
+  mint: '#BBE6D5',
+  gray: '#748497',
   BOLD_TEXT_COLOR: '#093E65',
   MEDIUM_TEXT_COLOR: '#748497',
   GREY_TEXT: '#aaa',
@@ -8,3 +13,5 @@ export const Colors = {
   BOX_SHADOW: '3px 4px 4px 0 grey',
   ACTIVE_COLOR: '#81D8CF',
 };
+
+export default Colors;
