be9cc7b0bca878e1a4448301ac49720ea7fe7a4a
= <younseu2@gmail.com>
2024-05-03 15:56:10 +0900

    Docs: gitignore 수정, babel.config.js 초기화

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-	presets: ['module:metro-react-native-babel-preset'],
-	plugins: [
-		// react-native-dotenv 
-		[
-			'module:react-native-dotenv',
-			// Development 환경 파일 설정
-			{
-				"envName": "APP_ENV",
-				"moduleName": "@env",
-				"path": ".env.local",
-				"blocklist": null,
-				"allowlist": null,
-				"blacklist": null, // DEPRECATED
-				"whitelist": null, // DEPRECATED
-				"safe": false,
-				"allowUndefined": true,
-			},
-			'react-native-dotenv-1', // 고유한 이름 부여
-		],
-	]
-}
+  presets: ['module:@react-native/babel-preset'],
+};
