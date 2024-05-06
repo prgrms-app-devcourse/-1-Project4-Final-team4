79c6b98a11cb1d7f5c93dfbd2be05c552abf51ae
= <younseu2@gmail.com>
2024-05-06 17:50:15 +0900

    Feat: 로그인 성공 시 maintab으로 이동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,6 +19,7 @@ const Login = () => {
     try {
       const response = await signInWithEmailAndPassword(auth, emailValue, passwordValue);
       console.log(response);
+      navigation.replace('MainTab');
     }
     catch(error) {
       console.log(error);
