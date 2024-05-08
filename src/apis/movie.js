abfd04265eda2c18497670252aceaf48b863be1b
Zero <jgm262@naver.com>
2024-05-08 11:09:50 +0900

    Feat: 맛집, 오락, 영화, 공연 API 구현

---
+++ src/apis/movie.js
@@ -0,0 +1,31 @@
+import {create} from 'apisauce';
+
+const baseURL = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice';
+
+const movieAPI = create({
+  baseURL,
+});
+
+const authKey = process.env.REACT_APP_MOVIE_API_KEY;
+const today = new Date();
+const curDate = `${today.getFullYear()}${String(today.getMonth() + 1).padStart(
+  2,
+  '0',
+)}${String(today.getDate() - 1).padStart(2, '0')}`;
+
+export const getMovie = async () => {
+  try {
+    const res = await movieAPI.get(
+      `/searchDailyBoxOfficeList.json?key=${authKey}&targetDt=${curDate}`,
+    );
+    if (res.ok) {
+      const {data} = res;
+      return data;
+    } else {
+      console.error('Error : ', res.problem);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error : ', error);
+  }
+};
