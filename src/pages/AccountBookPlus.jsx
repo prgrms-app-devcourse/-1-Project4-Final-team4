d402978e2914f55ed8d2c2533ed1e2f66258fcfa
Moon <mat437992@gmail.com>
2024-05-16 12:47:52 +0900

    Feat: 최근 소비내역 삭제 버튼 및 스타일 조정

--- src/pages/AccountBookPlus.jsx
+++ src/pages/AccountBookPlus.jsx
@@ -12,18 +12,20 @@ import React, {useState, useEffect} from 'react';
 import {Colors} from '../utils/Colors';
 import BasicHeader from '../components/BasicHeader';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import LunchIcon from 'react-native-vector-icons/MaterialIcons';
+import ShoppingIcon from 'react-native-vector-icons/MaterialIcons';
 
 const dummyData = [
   {
     id: 1,
-    title: '산으로 간 고등어',
+    title: '치킨',
     Price: 15000,
     category: '식비',
     img: 'https://source.unsplash.com/random.jpg​',
   },
   {
     id: 2,
-    title: '산으로 간 연어',
+    title: '초밥',
     Price: 50000,
     category: '식비',
     img: 'https://source.unsplash.com/random.jpg​',
@@ -37,14 +39,14 @@ const dummyData = [
   },
   {
     id: 4,
-    title: '산으로 간 삼치',
+    title: '존윅4',
     Price: 36000,
     category: '영화',
     img: 'https://source.unsplash.com/random.jpg​',
   },
   {
     id: 5,
-    title: '산으로 간 삼치',
+    title: '갈비찜',
     Price: 35000,
     category: '식비',
     img: 'https://source.unsplash.com/random.jpg​',
@@ -59,9 +61,12 @@ const dummyData = [
 ];
 
 const AccountBookPlus = ({navigation}) => {
+  // 데이터 리스트
   const [data, setData] = useState(dummyData);
-  const [totalPrice, setTotalPrice] = useState(0);
+  //총 합계금액 계산식
+  const totalPrice = data.reduce((total, item) => total + item.Price, 0);
 
+  //항목 삭제
   const deleteHandle = index => {
     const newData = [...data];
     newData.splice(index, 1);
@@ -80,64 +85,74 @@ const AccountBookPlus = ({navigation}) => {
             fontSize: 20,
             color: Colors.black,
             marginHorizontal: 20,
-            borderBottomWidth: 3,
+            borderBottomWidth: 1,
+            height: 40,
           }}>
           최근 소비내역
         </Text>
       </View>
-      <View style={{marginTop: 20}}>
+      <View
+        style={{
+          marginTop: 20,
+          flex: 1,
+        }}>
         <FlatList
-          data={dummyData}
-          renderItem={data => {
+          data={data}
+          renderItem={({item, index}) => {
+            // 카테고리에 따라 아이콘 분류
+            let categoryIcon;
+            if (item.category === '영화') {
+              categoryIcon = (
+                <Icon name={'movie-open'} size={55} color={Colors.grey} />
+              );
+            } else if (item.category === '식비') {
+              categoryIcon = (
+                <LunchIcon
+                  name={'lunch-dining'}
+                  size={55}
+                  color={Colors.grey}
+                />
+              );
+            } else if (item.category === '쇼핑') {
+              categoryIcon = (
+                <ShoppingIcon
+                  name={'shopping-cart'}
+                  size={55}
+                  color={Colors.grey}
+                />
+              );
+            } else {
+              // 다른 카테고리에 따른 아이콘 표시
+              categoryIcon = (
+                <Icon name={'bus'} size={55} color={Colors.grey} />
+              );
+            }
             return (
               <View style={styles.ContentWrapper}>
-                <Image
+                {categoryIcon}
+                <Text style={{fontSize: 15, color: 'black'}}>{item.title}</Text>
+                <Text
                   style={{
-                    width: 60,
-                    height: 60,
-                    borderRadius: 20,
-                    marginBottom: 36,
-                  }}
-                  src={data.item.img}
-                />
-                <View
-                  style={{
-                    flexDirection: 'column',
-                    marginLeft: 24,
-                    gap: 9,
+                    fontSize: 15,
+                    color: 'black',
                   }}>
-                  <Text style={{fontSize: 15, color: 'black'}}>
-                    {data.item.title}
-                  </Text>
-                  <Text
-                    style={{
-                      marginRight: 43,
-                      fontSize: 12,
-                      color: 'black',
-                    }}>
-                    {data.item.Price} 원
-                  </Text>
-                </View>
+                  {item.Price.toLocaleString()} 원
+                </Text>
+
                 <Text style={{fontSize: 15, color: 'black'}}>
-                  {data.item.category}
+                  {item.category}
                 </Text>
                 <View
                   style={{
-                    flexDirection: 'row',
-                    justifyContent: 'flex-end',
-                    marginLeft: 50,
-                    gap: 20,
-                    marginRight: 0,
+                    right: 30,
+                    position: 'absolute',
                   }}>
-                  <TouchableOpacity>
+                  <TouchableOpacity onPress={() => deleteHandle(index)}>
                     <Icon
-                      name="plus-circle-outline"
+                      name="close-circle-outline"
                       size={28}
-                      color={Colors.grey}
+                      color={Colors.main}
                     />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Icon name="close-circle-outline" size={28} color="red" />
                   </TouchableOpacity>
                 </View>
               </View>
@@ -150,6 +165,7 @@ const AccountBookPlus = ({navigation}) => {
           justifyContent: 'flex-end',
           flexDirection: 'row',
           marginRight: 20,
+          flex: 0.1,
         }}>
         <Text
           style={{
@@ -157,7 +173,7 @@ const AccountBookPlus = ({navigation}) => {
             fontSize: 20,
             color: Colors.black,
           }}>
-          총 {totalPrice} 원
+          총 {totalPrice.toLocaleString()} 원
         </Text>
       </View>
     </SafeAreaView>
@@ -179,12 +195,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   ContentWrapper: {
-    flex: 1,
     flexDirection: 'row',
     marginLeft: 20,
     marginTop: 10,
-    gap: 10,
+    gap: 20,
     justifyContent: 'flex-start',
+    width: 400,
+    height: 60,
+    alignItems: 'center',
   },
 });
 
